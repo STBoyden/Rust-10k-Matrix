3f213a84c0ec9612032acd02bc94a82e74217dcd
STBoyden <stboyden@stboyden.xyz>
2020-07-03 03:15:51 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+#![feature(box_syntax)]
+
+use rand::Rng;
+use rand_xorshift::XorShiftRng;
+use rayon::prelude::*;
+use std::time::SystemTime;
+
+fn main() {
+    let clock = SystemTime::now();
+
+    loop {
+        let loop_clock = SystemTime::now();
+        let mut matrix = box [[0u8; 10000]; 10000];
+
+        matrix.par_iter_mut().for_each(|matrix_row| {
+            matrix_row.par_iter_mut().for_each(|value| {
+                let seed = clock
+                    .elapsed()
+                    .expect("Could not get elapsed time since program start")
+                    .as_micros()
+                    .to_ne_bytes();
+
+                let mut rng: XorShiftRng = rand::SeedableRng::from_seed(seed);
+                *value = rng.gen();
+            })
+        });
+
+        let elapsed = loop_clock
+            .elapsed()
+            .expect("Could not calculate differences")
+            .as_secs_f64();
+
+        println!("{}", elapsed);
+    }
+}
