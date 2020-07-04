0041ef5acb041ab74928c4dfcaa0d5944659bf9a
STBoyden <stboyden@stboyden.xyz>
2020-07-04 01:14:13 +0100

    Modify both halves of the matrix in parallel.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ use rand_xorshift::XorShiftRng;
 use rayon::prelude::*;
 use std::time::SystemTime;
 
+fn change_value(value: &mut u8, clock: &SystemTime) {
+    let seed = clock
+        .elapsed()
+        .expect("Could not get elapsed time")
+        .as_micros()
+        .to_ne_bytes();
+    let mut rng: XorShiftRng = rand::SeedableRng::from_seed(seed);
+    *value = rng.gen();
+}
+
 fn main() {
     let clock = SystemTime::now();
 
@@ -12,18 +22,24 @@ fn main() {
         let loop_clock = SystemTime::now();
         let mut matrix = box [[0u8; 10000]; 10000];
 
-        matrix.par_iter_mut().for_each(|matrix_row| {
-            matrix_row.par_iter_mut().for_each(|value| {
-                let seed = clock
-                    .elapsed()
-                    .expect("Could not get elapsed time since program start")
-                    .as_micros()
-                    .to_ne_bytes();
+        let (matrix_first, matrix_last) = matrix.split_at_mut(5000);
 
-                let mut rng: XorShiftRng = rand::SeedableRng::from_seed(seed);
-                *value = rng.gen();
-            })
-        });
+        rayon::join(
+            || {
+                matrix_first.par_iter_mut().for_each(|matrix_row| {
+                    matrix_row.par_iter_mut().for_each(|value| {
+                        change_value(value, &clock);
+                    })
+                });
+            },
+            || {
+                matrix_last.par_iter_mut().for_each(|matrix_row| {
+                    matrix_row.par_iter_mut().for_each(|value| {
+                        change_value(value, &clock);
+                    })
+                });
+            },
+        );
 
         let elapsed = loop_clock
             .elapsed()
