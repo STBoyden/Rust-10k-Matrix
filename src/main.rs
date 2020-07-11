cd9aeb99b721439d551f73c7a6df5dcba2b997f6
STBoyden <stboyden@stboyden.xyz>
2020-07-11 17:35:58 +0100

    Replaces default allocator with jemalloc on Linux-based systems

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ use rand_xorshift::XorShiftRng;
 use rayon::prelude::*;
 use std::time::SystemTime;
 
+#[cfg(not(target_env = "msvc"))]
+use jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 fn change_value(value: &mut u8, clock: &SystemTime) {
     let seed = clock
         .elapsed()
