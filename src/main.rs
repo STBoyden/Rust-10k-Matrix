c64c36c802de2af08a15c6f16ddda844effff803
STBoyden <stboyden@stboyden.xyz>
2020-08-13 00:40:50 +0100

    Tidied program
    Got times down to 0.6 on i7 8600K.

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,56 @@
 #![feature(box_syntax)]
 
-use rand::Rng;
-use rand_xorshift::XorShiftRng;
+use num_cpus;
+use oorandom::Rand32;
 use rayon::prelude::*;
-// use std::sync::{Arc, Mutex};
 use std::time::SystemTime;
 
 #[macro_use]
 extern crate lazy_static;
 
-#[cfg(not(target_env = "msvc"))]
-use jemallocator::Jemalloc;
-
-#[cfg(not(target_env = "msvc"))]
-#[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
-
 lazy_static! {
     static ref CLOCK: SystemTime = SystemTime::now();
 }
 
-fn change_value(value: &mut u8) {
-    let seed = CLOCK
-        .elapsed()
-        .expect("Could not get elapsed time")
-        .as_micros()
-        .to_ne_bytes();
-    let mut rng: XorShiftRng = rand::SeedableRng::from_seed(seed);
-    *value = rng.gen();
-}
-
 fn main() {
-    let mut matrix = box [[0u8; 10000]; 10000];
-    loop {
-        let loop_clock = SystemTime::now();
+    const SIZE: usize = 10000;
+    let mut matrix = box [0u8; SIZE * SIZE];
+    let pool = rayon::ThreadPoolBuilder::new().build().unwrap();
+    let mut sum = 0.0;
+    let amt = 10;
 
-        matrix.splitn_mut(16, |_| true).for_each(|split| {
-            split.par_iter_mut().for_each(|matrix_row| {
-                matrix_row.iter_mut().for_each(|x| {
-                    change_value(x);
-                });
+    println!(
+        "\t{} Physical Cores | {} Logical Cores",
+        num_cpus::get_physical(),
+        num_cpus::get()
+    );
+
+    println!(
+        "Running {} iterations on {} threads...\n",
+        amt,
+        pool.current_num_threads()
+    );
+
+    let _ = pool.install(|| {
+        for i in 0..amt {
+            let loop_clock = SystemTime::now();
+
+            matrix.par_iter_mut().for_each(|x| {
+                let seed = CLOCK.elapsed().unwrap().as_secs();
+                let mut rng = Rand32::new(seed);
+                *x = rng.rand_u32() as u8;
             });
-        });
 
-        let elapsed = loop_clock
-            .elapsed()
-            .expect("Could not calculate differences")
-            .as_secs_f64();
+            let elapsed = loop_clock
+                .elapsed()
+                .expect("Could not calculate differences")
+                .as_secs_f64();
 
-        println!("{}", elapsed);
-    }
+            println!("[{}]\tTook: {:.4}s", i + 1, elapsed);
+
+            sum += elapsed;
+        }
+    });
+
+    println!("\nAverage: {:.4}s", sum / amt as f64);
 }
